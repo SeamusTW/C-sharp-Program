a806377d29010daf5a961791f478ff045ea1e5f2
seanh <seanh@HP-C46516F45FB5>
2021-10-08 11:42:30 +0100

    Add project files.

---
+++ my C sharp program/Car.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_C_sharp_program
+{
+    
+       
+     class Car
+     {
+        private string _modelName;
+        private string _color;    
+        public enum Colors
+        {
+        
+            red,blue ,green 
+
+        }
+        public Car(string modelName,Colors colors)
+        {
+            this._modelName = modelName;
+            this._color = Convert.ToString(colors);
+        }
+        public enum CarAttributes
+        {
+        
+            model = 1,
+            color = 2,
+            owner = 3
+
+        }
+        public string returnPrivateParameters(CarAttributes carAttribs)
+        {
+            
+            switch (Convert.ToInt32(carAttribs))
+            {
+               case 1:
+                    return _modelName;
+                    
+               case 2:
+                    return _color;
+                   
+            }
+            return "null";
+
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("model:" + returnPrivateParameters(CarAttributes.model));
+            Console.WriteLine("color:" + returnPrivateParameters(CarAttributes.color));
+        }
+        static void Main()
+        {
+
+            Car MyCar = new Car("Ford",Colors.red);
+            Console.Write(MyCar._color);
+            Console.ReadLine();
+
+        }
+
+     }
+
+     
+        
+            
+        
+
+
+    
+}
---
+++ my C sharp program/main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_C_sharp_program
+{
+
+    class main
+    {
+
+        static void Main()
+        { 
+        
+
+        
+        }
+           
+        
+
+
+    }
+}
